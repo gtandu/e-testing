4b261cfff0d1ae232f12e4d7a0bf3a2ac2466bb6
gtandu <tbrandon91@hotmail.fr>
2018-01-30 13:48:22 +0100

    Ajout back spring rest

---
+++ e-testing-rest/src/main/java/fr/etesting/etesting/ETestingApplication.java
@@ -0,0 +1,12 @@
+package fr.etesting.etesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETestingApplication.class, args);
+	}
+}
---
+++ e-testing-rest/src/test/java/fr/etesting/etesting/ETestingApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.etesting.etesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ETestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
