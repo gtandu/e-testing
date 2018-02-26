395938215191d7e170ee31e84828f3edc5c76802
cmapella <mapella.corentin@gmail.com>
2018-02-26 10:36:54 +0100

    #13

--- e-testing-front/src/app/login/login.component.ts
+++ e-testing-front/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthentificationService } from "../services/authentification.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  model: any = {};
+  errorMsg: String;
+  loader: boolean = false;
+  constructor(private router: Router, private authService: AuthentificationService) { }
 
   ngOnInit() {
+    this.authService.logout();
+  }
+
+  login(){
+    console.log(this.model);
+
+    this.authService.login(this.model.email, this.model.password).subscribe(
+      result => {
+        if (result === true) {
+          // login successful
+          console.log("TOKEN OK REDIRECT APP");
+          this.errorMsg = null;
+          this.loader = true;
+          this.router.navigate(['/localhost/home']);
+        } else {
+          // login failed
+          console.log("ERROR LOGIN");
+        }
+      },
+      error => {
+        if(error.status === 401){
+          this.errorMsg = "L'email ou le mot de passe est incorrect !"
+        }
+        else{
+          this.errorMsg = "Une erreur technique est survenue !"
+        }
+      });
   }
 
 }
