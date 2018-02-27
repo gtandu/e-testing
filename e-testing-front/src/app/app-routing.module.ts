65f02c7d9f40fa0a4cf38ee5f7cb2ae5818e3ebb
cmapella <mapella.corentin@gmail.com>
2018-02-27 11:24:44 +0100

    fix merge #13

---
+++ e-testing-front/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule }              from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+// routes
+const appRoutes: Routes = [
+	{ path: 'login', component: LoginComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+@NgModule({
+	imports: [
+		RouterModule.forRoot(appRoutes)
+	],
+	exports: [
+		RouterModule
+	]
+})
+export class AppRoutingModule { }
