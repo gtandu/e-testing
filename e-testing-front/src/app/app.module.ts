ade2a8df6ff0645b9697877d0058fb1203287e12
cmapella <mapella.corentin@gmail.com>
2018-02-27 09:24:09 +0100

    Commit to pull rebase gtandu data #13

--- e-testing-front/src/app/app.module.ts
+++ e-testing-front/src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AuthentificationService } from './services/authentification.service';
-
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthentificationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
