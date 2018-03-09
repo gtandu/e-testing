987089c504ea0e89396fd73aaa29681cb1fb3949
cmapella <mapella.corentin@gmail.com>
2018-03-09 18:20:54 +0100

    fix rebase #19

--- e-testing-front/src/app/app-routing.module.ts
+++ e-testing-front/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './etesting/home/home.component';
+import { FileUploadComponent } from './etesting/admin/fileupload/fileupload.component';
+import { QcmComponent } from './etesting/qcm/qcm.component';
 
 // routes
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'editqcm', component: FileUploadComponent },
+  { path: 'qcm', component: QcmComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
