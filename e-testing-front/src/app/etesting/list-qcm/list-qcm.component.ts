b100d69007ff89a5ec558b6195be406a8e2554de
cmapella <mapella.corentin@gmail.com>
2018-03-10 09:38:50 +0100

    Select a qcm #19

--- e-testing-front/src/app/etesting/list-qcm/list-qcm.component.ts
+++ e-testing-front/src/app/etesting/list-qcm/list-qcm.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Qcm } from '../../models/qcm';
 import { QcmService } from '../../services/qcm/qcm.service';
+import { Router } from '@angular/router';
+import { Paths } from '../../models/paths';
 
 @Component({
   selector: 'app-list-qcm',
@@ -10,13 +12,20 @@ import { QcmService } from '../../services/qcm/qcm.service';
 export class ListQcmComponent implements OnInit {
 
   qcms : Qcm[];
+  qcm : Qcm;
+  id : number;
 
-  constructor(private qcmService: QcmService) { }
+  constructor(private qcmService: QcmService, private router: Router) { }
 
   ngOnInit() {
     this.qcmService.getAllQcm().subscribe(qcm => {
       this.qcms = qcm;
     });
+  }
+
+  takeQcm(id){
+    console.log(id);
+    this.router.navigate(['etesting/' + Paths.QCM + `/${id}`]);
   }
 
 }
